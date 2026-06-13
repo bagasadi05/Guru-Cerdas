71fce09c1fec8be8a05fde86392c6412e6f46bcb
Bagas Adi <bagasadi05@gmail.com>
2026-06-13 22:26:37 +0700

    chore: disable vite server watch polling to fix dev server hangs on Windows

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,6 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       host: '0.0.0.0',
-      watch: {
-        usePolling: true,
-      },
     },
     plugins: [
       react(),
